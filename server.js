2b564cd19652fdbfcfea3fbf45f93e4c8bcfe393
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-05-29 20:18:16 +0300

    update gitignore

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-// NtAe5AxKE76MnQHf;
-
-const DB_HOST =
-  "mongodb+srv://Tolik2023:NtAe5AxKE76MnQHf@cluster0.6uflqwo.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose
   .connect(DB_HOST)
